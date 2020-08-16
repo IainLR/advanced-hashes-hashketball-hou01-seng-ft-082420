e1ac146a87f350f2cbf1094b57226ae599dbaf0e
Iain Robertson <iainrobertzon@gmail.com>
2020-08-16 15:16:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,10 +196,10 @@ def big_shoe_rebounds
   big_ol_foot = 0
   rebounds = 0 
   game_hash.values.each do |team| 
-    team[:players].each do |tall_person, value|
-      if value[:shoe] > big_ol_foot 
-        big_ol_foot = vlaue[:shoe]
-        rebounds = value[:rebounds]
+    team[:players].each do |tall_person|
+      if tall_person[:shoe] > big_ol_foot 
+        big_ol_foot = tall_person[:shoe]
+        rebounds = tall_person[:rebounds]
       end 
     end 
     return rebounds
